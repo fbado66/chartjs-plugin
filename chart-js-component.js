03601e2e22425daf2f1b0c634b69ffbe13e65324
fbado66 <fbado66@gmail.com>
2021-02-22 03:25:44 -0500

    displaying all charts and changes with respect to both axis completed

--- chart-js-component.js
+++ chart-js-component.js
@@ -2,18 +2,22 @@
 //   ---------- Global variables to help when appending the information onto the DOM
 let headers = document.querySelector('.main_headers')
 let table_body = document.querySelector('.table_body')
+let selectX_axis = document.querySelector('.selectX')
+let selectY_axis = document.querySelector('.selectY')
 
 
 // -------- Making a fetch request for the information --------------
 
 // Holds all the information 
 alldata = []
+let titles_on_data = []
 
 fetch(url)
 .then(res => res.json())
 .then(dataset => {
     alldata.push(dataset)
     let table_headers = (Object.keys(dataset[0]))
+    titles_on_data.push(Object.keys(dataset[0]))
 
     // ------------- CREATING THE TABLE --------------------- 
 
@@ -22,6 +26,20 @@ fetch(url)
             let tableHeader = document.createElement('th')
                 tableHeader.innerText = header
                 headers.append(tableHeader)
+
+                // Create the options for the select X axis on the graph
+                let XaxisOption = document.createElement('option')
+                    XaxisOption.value = header
+                    XaxisOption.innerText = header
+                    selectX_axis.append(XaxisOption)
+
+
+                // Create the options for the select Y axis on the graph
+                let YaxisOption = document.createElement('option')
+                    YaxisOption.value = header
+                    YaxisOption.innerText = header
+                    selectY_axis.append(YaxisOption)
+
         })
 
         // -------------- Building each row for the table ---------------------
@@ -40,7 +58,9 @@ fetch(url)
 })
 
 
- // ----------DEFININGG THE X-AXIS OPTIONS -------- 
+ // ----------DEFINING THE AXIS ARRAY OPTIONS -------- 
+//  For this section, I wanted to create each one of them dinamically, with the use of a loop. But to avoid the use of Vanilla.Js and risking the chance of not getting the array populated with the information in time, I opted for this static approach 
+
 let rowid = []
 let airlines = []
 let avail_seat_km_per_week = []
@@ -50,11 +70,12 @@ let fatalities_85_99 = []
 let incidents_00_14 = []
 let fatal_accidents_00_14 = []
 let fatalitites_00_14 = []
-setTimeout(() => {
 
+// I am using a setTimeOUt function to ensure the program runs and populate the information prior calling on it
+
+setTimeout(() => {
     // ----- set the numbers of all columns in the table ----------------
     let allColumns = Object.values(alldata[0][0]).length
-
 
     // ---I could try to use something like this to generate the variables for each column dynamically ----- 
 
@@ -85,72 +106,171 @@ setTimeout(() => {
 
             // }
         }
-    })
-
-
-    
+    }) 
 }, 100);
 
-console.log(rowid)
-console.log(airlines)
-console.log(fatalitites_00_14)
+// console.log(titles_on_data)
+// console.log(rowid)
+// console.log(airlines)
+// console.log(fatalitites_00_14)
+
+//  with more time I would make this into a helper function, and re-use for both axis
+
+// --- DYNAMICALLY CHANGE ON X AXIS --- 
+let xSelector = document.querySelector('.selectX')
+xSelector.addEventListener('change', (evt) => {
+    let holdX_value = evt.target.value
+    if (holdX_value == 'rowid') {
+        config.data.labels = rowid
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'rowid'
+    } else if(holdX_value == 'airline') {
+        config.data.labels = airlines
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'airlines'
+    } else if(holdX_value == 'avail_seat_km_per_week') {
+        config.data.labels = avail_seat_km_per_week
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'vail_seat_km_per_week'
+    } else if(holdX_value == 'incidents_85_99') {
+        config.data.labels = incidents_85_99
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'incidents_85_99'
+    } else if(holdX_value == 'fatal_accidents_85_99') {
+        config.data.labels = fatal_accidents_85_99
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'fatal_accidents_85_99'
+    } else if(holdX_value == 'fatalities_85_99') {
+        config.data.labels = fatalities_85_99
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'fatalities_85_99'
+    } else if(holdX_value == 'incidents_00_14') {
+        config.data.labels = incidents_00_14
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'incidents_00_14'
+    } else if(holdX_value == 'fatal_accidents_00_14') {
+        config.data.labels = fatal_accidents_00_14
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'fatal_accidents_00_14'
+    } else if(holdX_value == 'atalities_00_14') {
+        config.data.labels = atalities_00_14
+        config.options.scales.xAxes[0].scaleLabel.labelString = 'fatalities_00_14'
+    }
+    myChart.update()
+})
+
+// --- DYNAMICALLY CHANGE ON Y AXIS --- 
+let ySelector = document.querySelector('.selectY')
+ySelector.addEventListener('change', (evt) => {
+    let holdY_value = evt.target.value
+    if (holdY_value == 'rowid') {
+        config.data.datasets[0].data = rowid
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'rowid'
+    } else if(holdY_value == 'airline') {
+        config.data.datasets[0].data = airlines
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'airlines'
+    } else if(holdY_value == 'avail_seat_km_per_week') {
+        config.data.datasets[0].data = avail_seat_km_per_week
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'vail_seat_km_per_week'
+    } else if(holdY_value == 'incidents_85_99') {
+        config.data.datasets[0].data = incidents_85_99
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'incidents_85_99'
+    } else if(holdY_value == 'fatal_accidents_85_99') {
+        config.data.datasets[0].data = fatal_accidents_85_99
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'fatal_accidents_85_99'
+    } else if(holdY_value == 'fatalities_85_99') {
+        config.data.datasets[0].data = fatalities_85_99
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'fatalities_85_99'
+    } else if(holdY_value == 'incidents_00_14') {
+        config.data.datasets[0].data = incidents_00_14
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'incidents_00_14'
+    } else if(holdY_value == 'fatal_accidents_00_14') {
+        config.data.datasets[0].data = fatal_accidents_00_14
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'fatal_accidents_00_14'
+    } else if(holdY_value == 'atalities_00_14') {
+        config.data.datasets[0].data = atalities_00_14
+        config.options.scales.yAxes[0].scaleLabel.labelString = 'fatalities_00_14'
+    }
+    
+    myChart.update()
+})
 
 
-
-
+// SETUP / CONFIGURATION OF THE CHART.JS COMPONENT
 
 var config = {
     type: 'line',
     data: {
-      labels: ["June", "July", "August", "September", "October", "November", "December"],
+      labels: rowid,
       datasets: [{
-        label: "company1",
-        data: [2, 1, 2, 3, 4, 5, 6],
+        // ---- y axis -------
+        data: rowid,
         fill: false,
-        borderColor: "purple",
-        backgroundColor: "purple"
+        borderColor: "blue",
+        backgroundColor: "blue"
       }]
-    //   }, {
-    //     label: "company2",
-    //     data: [1, 2, 4, 8, 3, 2, 4],
-    //     fill: false,
-    //     borderColor: "green",
-    //     backgroundColor: "green"
-    //   }]
     },
     options: {
+        legend: {
+            display: true,
+            labels: {
+                fontColor: 'rgb(225, 99, 132)'
+            },
+        },
+        showLines: true,
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'rowid'
+                },
+                stacked: true
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'rowid'
+                },
+                stacked: true,
+                type: 'linear',
+                position: 'bottom',
+                ticks: {
+                    beginAtXZero: true
+                },
+            }]
+        },
       responsive: true,
-      
     }
   };
   
   var myChart;
 
+//   ----- Change the chart type  ---------
+
   let line = document.getElementById('line')
-  line.addEventListener('click', function() {change('line')})
+  line.addEventListener('click', function() {
+    config.options.showLines = true
+    change('line')})
   
   let bar = document.getElementById('bar')
-  bar.addEventListener('click', function() {change('bar')})
+  bar.addEventListener('click', function() {
+    config.options.showLines = true
+    change('bar')})
 
   let scatter = document.getElementById('scatter')
-  scatter.addEventListener('click', function() {change('scatter')})
+  scatter.addEventListener('click', function() {
+    config.options.showLines = !true
+    change('scatter')})
   
- 
-  
+
   function change(newType) {
+
     var ctx = document.getElementById("canvas").getContext("2d");
   
-    // Remove the old chart and all its event handles
+// Remove the chart and the event listners 
     if (myChart) {
       myChart.destroy();
     }
   
-    // Chart.js modifies the object you pass in. Pass a copy of the object so we can use the original object later
+    // Modify the Chart, pass a copy, with the new settings 
     var temp = config = (true, {}, config);
     temp.type = newType;
     myChart = new Chart(ctx, temp);
   };
-  
+
+
 
 
 
